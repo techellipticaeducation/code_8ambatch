faff8516df3133f049055a6b0338606250022381
techellipticaeducation <techellipticaeducation@gmail.com>
2020-07-08 09:46:38 +0530

    add Test1.java

--- src/test2/Test1.java
+++ src/test2/Test1.java
@@ -3,7 +3,13 @@ package test2;
 
 //   we can create as many as classes in a .java file
 //  free form
-public class Test1 {public static void main(String[] args) {}}class A{}
+public class Test1 {public static void main(String[] args) {
+	
+}
+}
+class A{
+	
+}
 
 class B{
 
