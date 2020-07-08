9f4bfb2e9fe373f11e7339e7e903eb2c735db8cb
techellipticaeducation <techellipticaeducation@gmail.com>
2020-07-08 09:45:50 +0530

    day1 commits

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -1,5 +1,13 @@
 package day1;
 
 public class Day1 {
+// .java -> compiler -> .class -> execute
+	// package -> folder
+	public static void main(String[] args) {
+		System.out.println("test");
 
+	// test4.test5
+		// test6.test7.test8
+		// test6/test7/test8
+	}
 }
---
+++ src/test2/Test1.java
@@ -0,0 +1,15 @@
+package test2;
+
+
+//   we can create as many as classes in a .java file
+//  free form
+public class Test1 {public static void main(String[] args) {}}class A{}
+
+class B{
+
+}
+
+// name of Java file Test1.java 
+// only Test1 class can be public 
+// other classes cannot be public
+
