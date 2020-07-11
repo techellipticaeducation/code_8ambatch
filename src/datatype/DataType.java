795322c0a621923a80b6049c7c0f3de9659c5042
techellipticaeducation <techellipticaeducation@gmail.com>
2020-07-11 23:20:49 +0530

    data type session

---
+++ src/datatype/DataType.java
@@ -0,0 +1,44 @@
+package datatype;
+
+public class DataType {
+	public static void main(String[] args) {
+		//  Datatype - 
+		// Numerical
+		
+		// byte -  1 byte 
+		// short - 2 byte = 16 bit , 2 power 16 = maximum 
+		// int - 4 byte = 32 bits , 2 power 32 
+		// long - 8 byte , 64 bits ,  2power 64 
+		
+		// what is byte? unit, 
+		// 1 byte - 8 bits 
+		
+		
+		// memory saving  roll number - 1000 
+		
+		int a = 106;  // Decimal
+		int i = 0106; // if you put 0 before any interger = octal
+		int j = 0XA6; //  if i put 0X = hexadecimal number System 
+		
+		//256 
+		// 256/2 = 128 
+		
+		// positive  0 to 127
+		// negative  -128 to 0
+		
+		//  -128 to 127
+		byte b1 = -128;
+		
+		System.out.println(Integer.toBinaryString(a));
+		
+		
+		System.out.println(a);
+		System.out.println(i);
+		System.out.println(j);
+		
+		
+		
+		
+		// Realtime sceanrio -> coding language
+	}
+}
