f1aefbb0bde64677bf8d3c25a7ba13ea30322b43
bhush <bhush@DESKTOP-B17T2O8.myfiosgateway.com>
2020-07-19 10:19:58 -0400

    assignments

---
+++ src/Assignments/Assignement_Day4.java
@@ -0,0 +1,81 @@
+package Assignments;
+
+public class Assignement_Day4 {
+	
+public static void main(String[] args) {
+//	
+//	Which variable notation is correct?
+//
+//			int 1st = 10; -- Incorrect
+//			int st1 = 10; -- Correct
+//			int $12 = 34; -- Correct
+//			int _123 = 45; -- Correct
+//			int r12@ = 34; -- Inorrect
+//			int =123 = 67;-- Incorrect
+//
+//
+//	Which variable notation is correct?
+//
+//			int i = 10; -- Correct
+//			int int = 20; -- Incorrect
+//			int goto = 30; -- inorrect
+//			int default = 40; -- Incorrect
+//			int class = 50; -- Incorrect
+//			int j = 70; -- Correct
+//
+//	Write Size and Range for all data types
+//			byte = 1 byte (8 Bits) store whole number -128 to 127
+//			short - 2 bytes
+//			int - 4 bytes
+//			long - 8 bytes
+//			float - 4 bytes
+//			double - 8 bytes
+//			char - 2 bytes
+//			boolean - 1 bit (Store True or flase value)
+//
+//			4. 
+//			int int = 10;
+//
+//			if yes, why?
+//			else if no, why?
+//
+//			Ans : No -- Can't create variable with same keyword.
+//
+//
+//	How many bit are there in bytes
+//			Ans 8 bits -- 1 byte
+//
+//			6. What maximum value, a byte can hold 
+//
+//			Ans =127
+//
+//
+//	Covert below Decimal to Octal
+//
+//			20 =24
+//			100 =604
+//			56==70
+//			78 =6321
+//			1453= 2655
+//			544=840
+//
+//
+//	Covert below Decimal to Binary
+//			20=10100
+//			39=100111
+//			49=110001
+//			28 =11100
+//			126 =1111110
+//			156 =10011100
+//
+//
+//
+////	Covert Below Decimal to hexadecimal
+//			245
+//			45
+//			67
+//			98
+//			712
+//
+}
+}
---
+++ src/Assignments/Assignment_numberswap.java
@@ -0,0 +1,30 @@
+	package Assignments;
+	
+	public class Assignment_numberswap {
+	
+		public static void main(String[] args) {	
+			
+			
+	// Without another variable defined
+			int i = 10;
+			int j = 20;
+			i=i+j;//30
+			j=i-j;//30-20
+			i=i-j;//30-10
+			System.out.println("i = "+i);
+			System.out.println("j = "+j);
+	}
+}
+	
+			
+    // With another variable defined
+//			
+//		int i = 10;
+//		int j = 20;
+//		int k = 10;
+//	    i=i+k;
+//	    j=j-k;
+//	    System.out.println("i = "+i);
+//		System.out.println("i = "+i);
+//	}
+//}
