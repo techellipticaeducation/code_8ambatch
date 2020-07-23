2b05554eb222dc60f504be396ad27928df4efaa3
nazne <nazne@DESKTOP-5BHBOUP>
2020-07-22 20:10:01 -0500

    integer to hexstring

---
+++ src/Assignment_6.java
@@ -0,0 +1,12 @@
+
+public class Assignment_6 {
+	
+public static void main(String args[]){  
+System.out.println(Integer.toHexString(245));  
+System.out.println(Integer.toHexString(45));  
+System.out.println(Integer.toHexString(67)); 
+System.out.println(Integar.toHexString(98));
+System.out.println(Integar.toHexString(712));
+
+}} 	
+
