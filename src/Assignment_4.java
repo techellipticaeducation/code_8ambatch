e8de561762f071c9a1d32cd2934d46dd36e4caa9
nazne <nazne@DESKTOP-5BHBOUP>
2020-07-22 19:00:10 -0500

    Assignment 4

---
+++ src/Assignment_4.java
@@ -0,0 +1,15 @@
+
+public class Assignment_4
+
+{public static void main(String args[]){
+	//to convert decimal value into octal 
+	
+	System.out.println(Integer.toOctalString(20));  
+	System.out.println(Integer.toOctalString(100));  
+	System.out.println(Integer.toOctalString(56));
+	
+	System.out.println(Integer.toOctalString(78));  
+	System.out.println(Integer.toOctalString(1453));  
+	System.out.println(Integer.toOctalString(544));
+	
+}}
