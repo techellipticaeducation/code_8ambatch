24c2979c9ccb6f49e6088b75df2947cafcc5bc87
techellipticaeducation <techellipticaeducation@gmail.com>
2020-07-24 09:11:35 +0530

    method Session and Comments

---
+++ src/methods/ExerciseDemo.java
@@ -0,0 +1,55 @@
+package methods;
+
+public class ExerciseDemo {
+
+	
+	
+	//  i want to create one method 
+	// name = square
+	// param = inputValue (1 value)
+	// return = int - which must be square of inputvalue
+	// 3 -> 9, 4 -> 16, 5-> 25
+	
+	// 3 Types
+	// Single line comment
+	// Multiline Comment
+	// Doc Comment
+	
+//	test1 - Single line comment
+ 	
+	/* - Multiline Comments
+	test1
+	test2
+	test3
+	test4
+	*/
+	
+	public static void main(String[] args) {
+		ExerciseDemo ex1 = new ExerciseDemo();
+		
+	}
+	
+	// How do we create Java Doc
+	
+	/**
+	 * hello i am doc comment
+	 * test1
+	 * test2
+	 * test3
+	 * test4
+	 */
+	int square(int inputValue) {
+		return inputValue*inputValue;
+	}
+	
+	/**
+	 * hello this is Sum method
+	 * @param inputValue
+	 * @param inputValue2
+	 * @return
+	 */
+	int sum(int inputValue, int inputValue2) {
+		return inputValue+inputValue2;
+	}
+	
+}
---
+++ src/methods/MethodDemo.java
@@ -0,0 +1,40 @@
+package methods;
+
+public class MethodDemo {
+	public static void main(String[] args) {
+		// Service
+
+		//	-> inputs  -> service -> Output
+
+
+	}
+
+	// add method
+	// when we give 2 values to add method , it sum up and return me the sum
+
+	// Straight forward
+
+
+	int add(int input1, int input2){
+		int sum = input1 + input2; // active
+		return sum;
+	}
+
+
+
+// return type of this method
+	boolean test(){
+		System.out.println("test");
+		return true;
+	}
+	
+	// method name - add
+	// params - input1(int) , input2(int)
+	// return type : int
+
+	// no param, double return 
+	double getPIValue(){
+		return 3.14;
+	}
+	// no params to that method
+}
