1ad14d7da9aa0be1e8ad658e969c88b44907caac
bhush <bhush@DESKTOP-B17T2O8.myfiosgateway.com>
2020-07-25 12:15:21 -0400

    commit

--- src/Assignments/Assignment_numberswap.java
+++ src/Assignments/Assignment_numberswap.java
@@ -3,19 +3,59 @@
 	public class Assignment_numberswap {
 	
 		public static void main(String[] args) {	
+// SUM ALL DIGIT VALUE
+//		int number = 12345;
+		
+///		 16
+		//  16 %10 
+		
+		// manually 
+		// 1+2+3+4+5+6
+// 		
+		// sum = 6 + 5 + 4 + 3 + 2 + 1
+		// number %10 = 6
+		// number / 10 = 1234
+		
+		//for(int i=1;i>=1;i++ )
+	  		
+//		int sum = 0;
+//			while(number!= 0) {
+//				System.out.println("========");
+//				System.out.println("number - "+number);
+//				System.out.println(number%10);
+//			sum = sum +	number % 10  ;
+//			System.out.println("sum - " + sum);
+//			number = number /10;
+			}
+//			sum = sum + number;
+//			System.out.println("sum is " +sum);
+//			
 			
 			
-	// Without another variable defined
-			int i = 10;
-			int j = 20;
-			i=i+j;//30
-			j=i-j;//30-20
-			i=i-j;//30-10
-			System.out.println("i = "+i);
-			System.out.println("j = "+j);
+// NUMBER REVERSE
+			int number1=547;
+			int a = 0;{
+			while(number1!=0) {
+				a =number1%10;
+				System.out.print(a);
+				number1=number1/10;
+			}
+			
+			
+		}
 	}
-}
-	
+			
+// NUMBER SWAP			
+	// Without another variable defined
+//			int i = 10;
+//			int j = 20;
+//			i=i+j;//30
+//			j=i-j;//30-20
+//			i=i-j;//30-10
+//			System.out.println("i = "+i);
+//			System.out.println("j = "+j);
+//	}
+//}
 			
     // With another variable defined
 //			
