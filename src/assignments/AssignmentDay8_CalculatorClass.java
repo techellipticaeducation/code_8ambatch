f2332286772a405e9adbfacc1ee4fc67212ba451
abhis <abhis@DESKTOP-IOREH40.myfiosgateway.com>
2020-07-26 19:48:30 -0400

    Assignment Day8 Practice

---
+++ src/assignments/AssignmentDay8_CalculatorClass.java
@@ -0,0 +1,35 @@
+package assignments;
+
+public class AssignmentDay8_CalculatorClass {
+
+	// Addition Method
+	public int add(int input1, int input2) {
+		int add = input1 + input2;
+		return add;
+	}
+
+	// Subtraction Method
+	public int diff(int input1, int input2) {
+		int diff = input1 + input2;
+		return diff;
+	}
+
+	// Multiplication Method
+	public int product(int input1, int input2) {
+		int product = input1 * input2;
+		return product;
+	}
+
+	// Quotient Method
+	public int quotient(int input1, int input2) {
+		int quotient = input1 / input2;
+		return quotient;
+	}
+
+	// Remainder Method
+	public int remainder(int input1, int input2) {
+		int remainder = input1 % input2;
+		return remainder;
+	}
+
+}
---
+++ src/assignments/AssignmentDay8_MainClass.java
@@ -0,0 +1,10 @@
+package assignments;
+
+public class AssignmentDay8_MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
