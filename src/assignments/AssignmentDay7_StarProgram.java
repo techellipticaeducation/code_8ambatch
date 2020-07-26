36377631bdf74a4a1ac6e0ddb4cee0a4dd21f893
abhis <abhis@DESKTOP-IOREH40.myfiosgateway.com>
2020-07-26 12:21:52 -0400

    Changes Assignment 7

--- src/assignments/AssignmentDay7_StarProgram.java
+++ src/assignments/AssignmentDay7_StarProgram.java
@@ -211,17 +211,17 @@ public class AssignmentDay7_StarProgram {
 //		}
 //System.out.println();
 //	Pattern 15
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= i; j++) {
-					if (i==3 && j==2 || i==4 && j==2 || i==4 && j==3 ) {
-					System.out.print(" ");	
-					}
-					else {
-						System.out.print('*');
-					}
-			}
-			System.out.println();
-		}
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 1; j <= i; j++) {
+//					if (i==3 && j==2 || i==4 && j==2 || i==4 && j==3 ) {
+//					System.out.print(" ");	
+//					}
+//					else {
+//						System.out.print('*');
+//					}
+//			}
+//			System.out.println();
+//		}
 //
 //System.out.println();
 //Pattern 16
