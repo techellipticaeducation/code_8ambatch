5085f01745ff752032e7e9bc807ee664bb80ab89
abhis <abhis@DESKTOP-IOREH40.myfiosgateway.com>
2020-08-12 11:31:08 -0400

    Adhoc Assignments

---
+++ src/assignments/AssignmentAdhoc1.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class AssignmentAdhoc1 {
+
+	public static void main(String args[]) {
+		
+//		String Test = "Mqp1rw84g5h";
+//		int temp = 0;
+//
+//		for (int i = 0; i < Test.length(); i++) {
+//			if (Character.isDigit(Test.charAt(i))) {
+//				temp = temp + (Test.charAt(i) - 48);
+//			}
+//
+//		}
+//		System.out.println("Sum of Digit in string : "+temp);
+
+//		String Test1 = "Hello ! How are you?";
+//		String testetString = "";
+//		for (int j = 0; j < Test1.length(); j++) {
+//			int temp1 = 0;
+//			for (int k = 0; k < Test1.length(); k++) {
+//				if (Test1.charAt(j) == Test1.charAt(k)) {
+//					temp1 = temp1 + 1;
+//				}
+//			}
+//			if(!testetString.contains(String.valueOf(Test1.charAt(j)))) {
+//			System.out.println(Test1.charAt(j) + " Occurance in string is : " + temp1);
+//			testetString = testetString + Test1.charAt(j);
+//			}
+//		}
+
+	}
+}
--- src/assignments/ExamClassTest.java
+++ src/assignments/ExamClassTest.java
@@ -4,7 +4,7 @@ public class ExamClassTest {
 
 	public static void main(String[] args) {
 
-	ExamClass.testPrimeNumber(4);
+	ExamClass.testPrimeNumber(5000);
 		
 	}
 }
