b2ec24a5fcc24e8269986fb2ce125e0aaabe0eee
abhis <abhis@DESKTOP-IOREH40.myfiosgateway.com>
2020-08-20 19:35:40 -0400

    AA

---
+++ src/assignments/AssignmentDay15.java
@@ -0,0 +1,39 @@
+package assignments;
+
+public class AssignmentDay15 {
+
+	
+//1) Types of Inheritance in Java
+//	 https://www.geeksforgeeks.org/inheritance-in-java/
+	
+	
+//2) Can you Prove Inheritance is unidirectional
+//	https://beginnersbook.com/2013/05/association/
+
+//3) What is extend keyword? How can we use extend keyword in inheritrance
+//	class Parent{
+//		void test() {
+//			System.out.println("Parent.test() method");
+//		}
+//	}
+//
+//	class Child extends Parent{
+//		void test1() {
+//			System.out.println("Child.test1() method");
+//		}
+//	}
+//
+//	class Main1{
+//		public static void main(String[] args) {
+//			Child ch=new Child();
+//			ch.test();
+//			ch.test1();
+//		}
+//	}
+
+//4) Write the difference
+//		/https://www.geeksforgeeks.org/object-oriented-programming-oops-concept-in-java/
+	
+}
+
+
